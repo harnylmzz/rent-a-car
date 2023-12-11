69b34794aa26a2cd5b028df1ccc890d4b8f09773
Harun Yılmaz <haruny232@aof.anadolu.edu.tr>
2023-12-11 13:56:24 +0300

    entities created

---
+++ src/main/java/com/tobeto/rentacar/entities/User.java
@@ -0,0 +1,32 @@
+package com.tobeto.rentacar.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "gsm")
+    private String gsm;
+}
