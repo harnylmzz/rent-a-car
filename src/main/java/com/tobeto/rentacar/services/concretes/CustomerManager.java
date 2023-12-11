1828240e02d9fa7b2cb6fe3cccce873d845a3ca2
Harun Yılmaz <haruny232@aof.anadolu.edu.tr>
2023-12-11 15:14:55 +0300

    services & controller added

---
+++ src/main/java/com/tobeto/rentacar/services/concretes/CustomerManager.java
@@ -0,0 +1,10 @@
+package com.tobeto.rentacar.services.concretes;
+
+import com.tobeto.rentacar.services.abstracts.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomerManager implements CustomerService {
+}
