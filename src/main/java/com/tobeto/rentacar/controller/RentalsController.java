3f63b99444a6e252b931376ce255de3ed64a7555
Taner Dökmetaş <tanerdokmets@gmail.com>
2023-12-14 22:15:58 +0300

    Controller bug fixed

--- src/main/java/com/tobeto/rentacar/controller/RentalsController.java
+++ src/main/java/com/tobeto/rentacar/controller/RentalsController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/brands")
+@RequestMapping("/api/rentals")
 public class RentalsController {
 }
