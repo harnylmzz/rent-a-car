93d8e5a656b46597bfc0223826880af495d07292
Taner Dökmetaş <tanerdokmets@gmail.com>
2023-12-14 22:15:58 +0300

    Controller bug fixed

--- src/main/java/com/tobeto/rentacar/controller/ModelsController.java
+++ src/main/java/com/tobeto/rentacar/controller/ModelsController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/brands")
+@RequestMapping("/api/models")
 @AllArgsConstructor
 public class ModelsController {
     private ModelService modelService;
