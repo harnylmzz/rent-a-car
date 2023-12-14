359297f354623ff60dbf252d7db34eb8756ab2dd
Harun Yılmaz <113914227+harnylmzz@users.noreply.github.com>
2023-12-14 20:49:07 +0300

    car tablosu isterleri tamamlandı
    Araba tablosundaki "kilometer" alanı 0'dan küçük olamaz.
    "Plate" alanı için Regex ile Türkiye plakasına uygunluk validasyonu eklenmelidir.
    Yıl bilgisi 2005-2024 arasında olmalıdır
    DailyPrice 0'dan küçük olamaz
    ModelId ve ColorId 0'dan küçük olamaz.
    Araç eklenirken plate kısmında verilen stringdeki tüm boşluklar kaldırılıp kaydedilmelidir ( örn : kullanıcının vereceği 34 ABC 06 => 34ABC06 olarak kaydedilmelidir)

--- src/main/java/com/tobeto/rentacar/services/concretes/CarManager.java
+++ src/main/java/com/tobeto/rentacar/services/concretes/CarManager.java
@@ -9,9 +9,11 @@ import com.tobeto.rentacar.services.dtos.requests.car.DeleteCarRequests;
 import com.tobeto.rentacar.services.dtos.requests.car.UpdateCarRequests;
 import com.tobeto.rentacar.services.dtos.responses.car.GetAllCarResponses;
 import com.tobeto.rentacar.services.dtos.responses.car.GetByIdCarResponses;
-import com.tobeto.rentacar.services.dtos.responses.employee.GetByIdEmployeeResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -21,12 +23,13 @@ import java.util.stream.Collectors;
 public class CarManager implements CarService {
     private CarRepository carRepository;
     private ModelMapperService modelMapperService;
+
     @Override
     public List<GetAllCarResponses> getAll() {
         List<Car> cars = carRepository.findAll();
         List<GetAllCarResponses> getAllCarResponses = cars.stream()
                 .map(car -> this.modelMapperService.forResponse()
-                        .map(car , GetAllCarResponses.class))
+                        .map(car, GetAllCarResponses.class))
                 .collect(Collectors.toList());
 
         return getAllCarResponses;
@@ -36,7 +39,7 @@ public class CarManager implements CarService {
     public GetByIdCarResponses getById(int id) {
         Car car = carRepository.findById(id).orElseThrow();
         GetByIdCarResponses getByIdCarResponses = this.modelMapperService.forResponse()
-                .map(car , GetByIdCarResponses.class);
+                .map(car, GetByIdCarResponses.class);
 
         return getByIdCarResponses;
     }
@@ -44,16 +47,32 @@ public class CarManager implements CarService {
     @Override
     public void add(CreateCarRequests createBrandRequests) {
         Car car = this.modelMapperService.forRequest()
-                .map(createBrandRequests , Car.class);
+                .map(createBrandRequests, Car.class);
+
+        if (car.getKilometer() < 0) {
+            throw new IllegalArgumentException("Kilometer field cannot be less than 0!");
+        }
+
+        String plate = createBrandRequests.getPlate().replaceAll("\\s+", "");
+        car.setPlate(plate);
+
+        int year = car.getYear();
+        if (year < 2005 || year > 2024) {
+            throw new IllegalArgumentException("Year information should be between 2005 and 2024.");
+        }
+
+        if (car.getPrice() < 0) {
+            throw new IllegalArgumentException("DailyPrice cannot be less than 0.");
+        }
+
         this.carRepository.save(car);
-
-
     }
+
 
     @Override
     public void update(UpdateCarRequests updateCarRequests) {
         Car car = this.modelMapperService.forRequest()
-                .map(updateCarRequests , Car.class);
+                .map(updateCarRequests, Car.class);
         car.setId(updateCarRequests.getId());
         car.setKilometer(updateCarRequests.getKilometer());
         car.setYear(updateCarRequests.getYear());
@@ -66,7 +85,7 @@ public class CarManager implements CarService {
     @Override
     public void delete(DeleteCarRequests deleteCarRequests) {
         Car car = this.modelMapperService.forRequest()
-                .map(deleteCarRequests , Car.class);
+                .map(deleteCarRequests, Car.class);
         this.carRepository.delete(car);
 
     }
--- src/main/java/com/tobeto/rentacar/services/dtos/requests/car/CreateCarRequests.java
+++ src/main/java/com/tobeto/rentacar/services/dtos/requests/car/CreateCarRequests.java
@@ -2,6 +2,7 @@ package com.tobeto.rentacar.services.dtos.requests.car;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,17 +13,15 @@ import lombok.NoArgsConstructor;
 public class CreateCarRequests {
 
     @NotNull(message = "Brand id is required")
-    @NotBlank(message = "Brand id is required")
     private int kilometer;
 
     @NotNull(message = "Year is required")
-    @NotBlank(message = "Year is required")
     private int year;
 
     @NotNull(message = "Price is required")
-    @NotBlank(message = "Price is required")
     private double price;
 
+    @Pattern(regexp = "^[0-9]{2}\\s[A-Z]{1,3}\\s[0-9]{2,3}$", message = "Please enter a valid Turkish plate number")
     @NotNull(message = "Plate is required")
     @NotBlank(message = "Plate is required")
     private String plate;
