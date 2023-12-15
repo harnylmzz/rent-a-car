614cf3fef9359249222b7184bfdaa42c1b202e64
ŞEHMUS EKİN <146784285+sehmusekin@users.noreply.github.com>
2023-12-15 16:12:49 +0300

    Rental crud işlemleri eklendi

--- src/main/java/com/tobeto/rentacar/controller/RentalsController.java
+++ src/main/java/com/tobeto/rentacar/controller/RentalsController.java
@@ -1,9 +1,52 @@
 package com.tobeto.rentacar.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.tobeto.rentacar.services.abstracts.RentalService;
+import com.tobeto.rentacar.services.dtos.requests.color.CreateColorRequests;
+import com.tobeto.rentacar.services.dtos.requests.color.DeleteColorRequests;
+import com.tobeto.rentacar.services.dtos.requests.color.UpdateColorRequests;
+import com.tobeto.rentacar.services.dtos.requests.rental.CreateRentalRequests;
+import com.tobeto.rentacar.services.dtos.requests.rental.DeleteRentalRequests;
+import com.tobeto.rentacar.services.dtos.requests.rental.UpdateRentalRequests;
+import com.tobeto.rentacar.services.dtos.responses.color.GetAllColorResponses;
+import com.tobeto.rentacar.services.dtos.responses.color.GetByIdColorResponses;
+import com.tobeto.rentacar.services.dtos.responses.rental.GetAllRentalResponses;
+import com.tobeto.rentacar.services.dtos.responses.rental.GetByIdRentalResponses;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/brands")
+@AllArgsConstructor
+@RequestMapping("/api/rentals")
 public class RentalsController {
+    private RentalService rentalService;
+
+    @GetMapping("/getAll")
+    public List<GetAllRentalResponses> getAll() {
+
+        return this.rentalService.getAll();
+    }
+
+    @GetMapping("/getById")
+    public GetByIdRentalResponses getById(
+            int id) {
+        return rentalService.getById(id);
+    }
+
+    @PostMapping("/add")
+    public void add(@RequestBody CreateRentalRequests createRentalRequests) {
+        this.rentalService.add(createRentalRequests);
+    }
+
+    @PutMapping("/update")
+    public void update(@RequestBody UpdateRentalRequests updateRentalRequests) {
+        this.rentalService.update(updateRentalRequests);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestBody DeleteRentalRequests deleteRentalRequests) {
+        this.rentalService.delete(deleteRentalRequests);
+    }
+
 }
