afbce3c2c4bdcef56891bb03fb3c5ae634411801
Harun Yılmaz <113914227+harnylmzz@users.noreply.github.com>
2024-01-21 14:19:41 +0300

    corporate customer controller added

--- src/main/java/com/tobeto/rentacar/services/dtos/requests/corporateCustomer/CreateCorporateCustomerRequests.java
+++ src/main/java/com/tobeto/rentacar/services/dtos/requests/corporateCustomer/CreateCorporateCustomerRequests.java
@@ -1,6 +1,8 @@
 package com.tobeto.rentacar.services.dtos.requests.corporateCustomer;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateCorporateCustomerRequests {
 
+    @NotBlank(message = "Company name is required")
+    @NotNull(message = "Company name is required")
     private String companyName;
 
+    @NotBlank(message = "Tax number is required")
+    @NotNull(message = "Tax number is required")
     private String taxNumber;
 }
