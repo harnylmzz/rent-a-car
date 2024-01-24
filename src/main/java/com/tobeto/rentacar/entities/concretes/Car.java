5120a18d9de1a20079619865b5f97d4ca87306b9
t4ner <tanerdokmets@gmail.com>
2024-01-25 02:11:55 +0300

    database data added
    database data added

--- src/main/java/com/tobeto/rentacar/entities/concretes/Car.java
+++ src/main/java/com/tobeto/rentacar/entities/concretes/Car.java
@@ -4,7 +4,6 @@ import com.tobeto.rentacar.entities.abstracts.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
