afed82583cb95e42f435d6c7db59f78763ee8219
ŞEHMUS EKİN <146784285+sehmusekin@users.noreply.github.com>
2024-02-01 17:57:56 +0300

    Car messages updated
    Car messages updated

--- src/main/java/com/tobeto/rentacar/services/constans/car/CarMessages.java
+++ src/main/java/com/tobeto/rentacar/services/constans/car/CarMessages.java
@@ -14,4 +14,14 @@ public class CarMessages {
     public static final String CAR_UPDATED = "Car updated";
     public static final String CAR_DELETED = "Car deleted";
     public static final String CAR_NOT_FOUND = "Car not found";
+
+    public static final String BRAND_ID_IS_REQUIRED= " Brand id is required";
+
+    public static final String YEAR_IS_REQUIRED =  "Year is required";
+
+    public static final String PRICE_IS_REQUIRED = "Price is required";
+
+    public static final String PLATE_IS_REQUIRED  = "Plate is required";
+
+    public static final String PLEASE_ENTER_A_VALID_TURKISH_PLATE_NUMBER = "Please enter a valid Turkish plate number";
 }
--- src/main/java/com/tobeto/rentacar/services/dtos/requests/car/CreateCarRequests.java
+++ src/main/java/com/tobeto/rentacar/services/dtos/requests/car/CreateCarRequests.java
@@ -1,5 +1,6 @@
 package com.tobeto.rentacar.services.dtos.requests.car;
 
+import com.tobeto.rentacar.services.constans.car.CarMessages;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -13,20 +14,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateCarRequests {
 
-    @NotNull(message = "Brand id is required")
+    @NotNull(message = CarMessages.BRAND_ID_IS_REQUIRED)
     private int kilometer;
 
-    @NotNull(message = "Year is required")
+    @NotNull(message = CarMessages.YEAR_IS_REQUIRED)
     private int year;
 
-    @NotNull(message = "Price is required")
+    @NotNull(message = CarMessages.PRICE_IS_REQUIRED)
     private double price;
 
     private String gearType;
 
-    @Pattern(regexp = "^[0-9]{2}\\s[A-Z]{1,3}\\s[0-9]{2,3}$", message = "Please enter a valid Turkish plate number")
-    @NotNull(message = "Plate is required")
-    @NotBlank(message = "Plate is required")
+    @Pattern(regexp = "^[0-9]{2}\\s[A-Z]{1,3}\\s[0-9]{2,3}$", message = CarMessages.PLEASE_ENTER_A_VALID_TURKISH_PLATE_NUMBER)
+    @NotNull(message = CarMessages.PLATE_IS_REQUIRED)
+    @NotBlank(message = CarMessages.PLATE_IS_REQUIRED)
     private String plate;
 
     private int modelId;
