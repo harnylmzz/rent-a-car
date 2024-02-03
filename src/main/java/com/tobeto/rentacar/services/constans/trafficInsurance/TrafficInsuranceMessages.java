2dfcfb74da81b092681da456f61f0942cdf4a0a6
ŞEHMUS EKİN <146784285+sehmusekin@users.noreply.github.com>
2024-02-03 11:10:13 +0300

    messages fıle uptade 3

---
+++ src/main/java/com/tobeto/rentacar/services/constans/trafficInsurance/TrafficInsuranceMessages.java
@@ -0,0 +1,13 @@
+package com.tobeto.rentacar.services.constans.trafficInsurance;
+
+
+
+public class TrafficInsuranceMessages {
+    public static final String TRAFFIC_INSURANCES_LISTED = "Traffic insurances listed.";
+
+    public static final String TRAFFIC_INSURANCE_ADDED = "Traffic insurance added.";
+
+    public static final String TRAFFIC_INSURANCE_UPDATED = "Traffic insurance updated.";
+
+    public static final String TRAFFIC_INSURANCE_DELETED = "Traffic insurance deleted.";
+}
