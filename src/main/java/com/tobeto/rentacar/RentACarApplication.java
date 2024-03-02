5f46eaf1066247e7948ee50c3a84a1eb3f1a0e22
Taner <tanerdokmets@gmail.com>
2024-03-02 15:12:48 +0300

    updated

--- src/main/java/com/tobeto/rentacar/RentACarApplication.java
+++ src/main/java/com/tobeto/rentacar/RentACarApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RentACarApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RentACarApplication.class, args);
     }
